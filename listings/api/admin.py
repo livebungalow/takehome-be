19fe37028cfcd024c0f806598f4dffe7c400da03
Virginia Donaire <vdonaire@wayfair.com>
2022-11-21 13:37:27 -0500

    Revert "first attept with many modifications"
    This reverts commit b48e6c034b8186d8e66e6c1e6cfbc528377e92b1.

--- listings/api/admin.py
+++ listings/api/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
 # TODO: Register your models here.
-from .models import Listings
-admin.site.register(Listings)
--- listings/listings/settings.py
+++ listings/listings/settings.py
@@ -131,6 +131,5 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'PAGE_SIZE': 10
+    ]
 }
