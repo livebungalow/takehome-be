8f0d378bbe9e45cc31969b50b2270b8913a68bd3
StevenMedeot <55761357+StevenMedeot@users.noreply.github.com>
2024-02-17 19:03:21 -0500

    Load data from CSV

--- listings/api/management/commands/import_house_data.py
+++ listings/api/management/commands/import_house_data.py
@@ -1,12 +1,66 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import models
+import csv
+from datetime import datetime
+
+from api import models as api_models
+
+column_conversions = {
+    "last_sold_date": "sale_date",
+    "property_size": "lot_size",
+    "zestimate_amount": "estimate_amount",
+    "zestimate_last_updated": "estimate_last_update",
+    "rentzestimate_amount": "rent_estimate_price",
+    "rentzestimate_last_updated": "rent_estimate_last_update",
+    "last_sold_price": "price",
+    "price": "market_price"
+}
+
+to_create_models = [
+    api_models.Home,
+    api_models.RentData,
+    api_models.SaleData,
+]
 
 class Command(BaseCommand):
     help = 'Imports data about houses'
 
     def add_arguments(self, parser):
-        # TODO: Add any arguments here
-        pass
+        parser.add_argument("file_path", type=str, help="Path to the CSV to load into the Database")
+
+    def get_fields(self, model):
+        return model._meta.fields
 
     def handle(self, *args, **options):
-        # TODO: implement your import command
-        self.stdout.write("Import to be implemented.")
+        csv_path = options["file_path"]
+        with open(csv_path, newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter=",")
+            column_data = {}
+            for row in reader:
+                if len(column_data) == 0:
+                    for i in range(len(row)):
+                        data = row[i].replace(' ', '')
+                        if data in column_conversions:
+                            data = column_conversions[data]
+                        column_data[data] = i
+                    continue
+                for model in to_create_models:
+                    kwargs = {}
+                    for field in self.get_fields(model):
+                        field_name = field.name
+                        if field_name in column_data:
+                            data = row[column_data[field_name]]
+                            if data and isinstance(field, models.fields.DateField):
+                                data = datetime.strptime(data, '%m/%d/%Y')
+                            kwargs[field_name] = data if data else None
+                        if model == api_models.Home and field_name == "id":
+                            data = row[column_data["zillow_id"]]
+                            kwargs["id"] = data if data else None
+                        
+                        if field_name == "home":
+                            data = row[column_data["zillow_id"]]
+                            kwargs["home_id"] = data if data else None
+                    model.objects.get_or_create(**kwargs)
+
+
+                
--- listings/api/models.py
+++ listings/api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from api import enums
 
-# TODO: Create your models here.
+class Home(models.Model):
+    link = models.URLField("Link", unique=True, null=False, blank=False)
+    address = models.CharField("Address", max_length=255, unique=True, null=False, blank=False)
+    city = models.CharField("City", max_length=255, null=False, blank=False)
+    state = models.CharField("State", max_length=2, null=False, blank=False)
+    zipcode = models.CharField("zipcode", max_length=255, null=False, blank=False)
+
+    bathrooms = models.FloatField("Bathrooms", default=None, null=True)
+    bedrooms = models.FloatField("Bedrooms")
+
+    year_built = models.IntegerField("Build Year", null=True, blank=True)
+    tax_year = models.IntegerField("Tax Year", default=None, null=True)
+    tax_value = models.FloatField("Tax Value", default=None, null=True)
+
+    home_size = models.IntegerField("Home Size", default=None, null=True)
+    lot_size = models.IntegerField("Lot Size", default=None, null=True)
+
+    area_unit = models.CharField("Area Unit", max_length=255, default="SqFt", null=False, blank=False)
+    home_type = models.CharField("Home Type", max_length=255, choices=enums.HomeType.choices(), null=False, blank=False)
+
+    estimate_amount = models.IntegerField("Estimate Amount", default=None, null=True)
+    estimate_last_update = models.DateField()
+    market_price = models.CharField("Area Unit", max_length=255, null=False, blank=False)
+
+
+class RentData(models.Model):
+    home = models.ForeignKey(Home, null=False, on_delete=models.CASCADE)
+
+    rent_price = models.CharField("Rent Price", max_length=255, null=True, blank=True)
+    rent_estimate_price = models.IntegerField("Rent Estimate", default=None, null=True)
+    rent_estimate_last_update = models.DateField(null=True)
+
+
+class SaleData(models.Model):
+    home = models.ForeignKey(Home, null=False, on_delete=models.CASCADE)
+    
+    listing_date = models.DateField(null=True)
+    sale_date = models.DateField(null=True)
+
+    price = models.IntegerField(null=True)
